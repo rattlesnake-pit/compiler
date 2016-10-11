dd32a6f70d54bc8c7eb0a4b8e03c53f7ebb86dd3
Antonio de Luna <tonydelun@gmail.com>
2016-10-11 16:24:03 -0700

    fix previous commit, added missing file

---
+++ parser_helper.c
@@ -0,0 +1,12 @@
+#include "compiler.h"
+
+int isDeclaration(enum TOKENS TOKEN) {
+  return TOKEN == INT_TYPE ||
+         TOKEN == CHAR_TYPE ||
+         TOKEN == FLOAT_TYPE ||
+         TOKEN == DOUBLE_TYPE;
+}
+
+int isAssignment(enum TOKENS TOKEN) {
+  return 0;
+}
